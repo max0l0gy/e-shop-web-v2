76659437d610d3ebde7830f954f878bf980b3898
max0l0gy <maxmorev@gmail.com>
2020-09-13 16:10:04 +0300

    fix(*):   feign client form data config request

--- src/main/java/ru/maxmorev/restful/eshop/feignclient/MinioRestApi.java
+++ src/main/java/ru/maxmorev/restful/eshop/feignclient/MinioRestApi.java
@@ -1,6 +1,5 @@
 package ru.maxmorev.restful.eshop.feignclient;
 
-import feign.Param;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,7 +10,7 @@ import ru.maxmorev.restful.eshop.feignclient.domain.FileUploadResponse;
 
 @FeignClient(name = "eshop-minio-api", url = "${external.minioApi.url}")
 public interface MinioRestApi {
-    @PostMapping(path ="/file/eshop" , consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(path ="/eshop" , consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     FileUploadResponse uploadFile(@RequestParam("key") String key,
-                                  @RequestPart("file") MultipartFile file);
+                                  @RequestParam("file") MultipartFile file);
 }
