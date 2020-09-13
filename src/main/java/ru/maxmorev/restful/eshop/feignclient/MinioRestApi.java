60605492d5168b1a56ae5d2ff8565f735541047b
max0l0gy <maxmorev@gmail.com>
2020-09-13 16:33:40 +0300

    fix(*):  feign client form data config experiment  removed: redundant code

--- src/main/java/ru/maxmorev/restful/eshop/feignclient/MinioRestApi.java
+++ src/main/java/ru/maxmorev/restful/eshop/feignclient/MinioRestApi.java
@@ -10,7 +10,7 @@ import ru.maxmorev.restful.eshop.feignclient.domain.FileUploadResponse;
 
 @FeignClient(name = "eshop-minio-api", url = "${external.minioApi.url}")
 public interface MinioRestApi {
-    @PostMapping(path ="/eshop" , consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(path ="/eshop", produces = MediaType.APPLICATION_JSON_VALUE)
     FileUploadResponse uploadFile(@RequestParam("key") String key,
-                                  @RequestPart("file") MultipartFile file);
+                                  @RequestParam("file") MultipartFile file);
 }
