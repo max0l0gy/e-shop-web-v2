cb4c290c11d392098279a1aff51060090ee69c1a
max0l0gy <maxmorev@gmail.com>
2021-02-09 22:41:14 +0300

    feature(*):   - Saving requested url at cookie

--- src/main/java/ru/maxmorev/restful/eshop/config/PrefixRedirectAuthenticationSuccessHandler.java
+++ src/main/java/ru/maxmorev/restful/eshop/config/PrefixRedirectAuthenticationSuccessHandler.java
@@ -8,8 +8,10 @@ import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
 
 @Slf4j
 public class PrefixRedirectAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
@@ -19,6 +21,8 @@ public class PrefixRedirectAuthenticationSuccessHandler implements Authenticatio
     private String webRoot;
     @Value("${web.host}")
     private String webHost;
+    @Value("${security.defaultRedirectPage}")
+    private String defaultRedirectPage;
 
     @Override
     @SneakyThrows
@@ -30,12 +34,19 @@ public class PrefixRedirectAuthenticationSuccessHandler implements Authenticatio
         log.info("targetUrl : {}", targetUrl);
         log.info("webRoot : {}", webRoot);
         log.info("webHost : {}", webHost);
-        String requestedUrl = request.getSession().getAttribute(REQUESTED_URL_BEFORE_LOGIN).toString();
+        String requestedUrl = getRedirectUrlFromCookies(request);
         log.info("REQUESTED_URL_BEFORE_LOGIN : {}", requestedUrl);
-        if (requestedUrl != null) {
-            String redirectedUrlWithWebPrefix = requestedUrl.replace(webHost, webRoot);
-            log.info("{} -> {}", requestedUrl, redirectedUrlWithWebPrefix);
-            redirectStrategy.sendRedirect(request, response, redirectedUrlWithWebPrefix);
-        }
+        redirectStrategy.sendRedirect(request, response, requestedUrl);
+    }
+
+    private String getRedirectUrlFromCookies(HttpServletRequest request) {
+        String requestedUrl =  Arrays.stream(request.getCookies())
+                .filter(cookie -> cookie.getName().equals(REQUESTED_URL_BEFORE_LOGIN))
+                .findFirst()
+                .map(Cookie::getValue).orElse(webHost + defaultRedirectPage);
+        log.info("Requested URL from Cookie: {}", requestedUrl);
+        String redirectedUrlWithWebPrefix = requestedUrl.replace(webHost, webRoot);
+        log.info("{} - adding prefix -> {}", requestedUrl, redirectedUrlWithWebPrefix);
+        return redirectedUrlWithWebPrefix;
     }
 }
--- src/main/java/ru/maxmorev/restful/eshop/web/AdminWebController.java
+++ src/main/java/ru/maxmorev/restful/eshop/web/AdminWebController.java
@@ -6,9 +6,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.util.Collections;
 
 import static ru.maxmorev.restful.eshop.config.PrefixRedirectAuthenticationSuccessHandler.REQUESTED_URL_BEFORE_LOGIN;
 
@@ -17,7 +18,7 @@ import static ru.maxmorev.restful.eshop.config.PrefixRedirectAuthenticationSucce
 public class AdminWebController {
 
     @GetMapping(path = {"/security/in/"})
-    public String securityPage(HttpServletRequest request, Model uiModel) {
+    public String securityPage(HttpServletRequest request, HttpServletResponse response, Model uiModel, Cookie loginCookie) {
         log.info("------------------------ securityPage ------------------------");
         HttpSession session = request.getSession();
         SavedRequest savedRequest = (SavedRequest) session.getAttribute("SPRING_SECURITY_SAVED_REQUEST");
@@ -26,8 +27,19 @@ public class AdminWebController {
             String redirectUrl = savedRequest.getRedirectUrl();
             log.info("savedRequest.getRedirectUrl() is {}", redirectUrl);
             request.getSession().setAttribute(REQUESTED_URL_BEFORE_LOGIN, redirectUrl);
+            setRequestPage(redirectUrl, response);
         }
         return "customer/login";
+    }
+
+    private void setRequestPage(String requestPage, HttpServletResponse response) {
+        Cookie requestedUrl = new Cookie(REQUESTED_URL_BEFORE_LOGIN, requestPage);
+        requestedUrl.setComment("Requested page for usability of our web shop UI. Thank you.");
+        requestedUrl.setMaxAge(60 * 60 * 24);//1 days in seconds
+        requestedUrl.setPath("/");
+        //newCartCookie.setDomain();
+        log.info("SETTING COOKIE");
+        response.addCookie(requestedUrl);
     }
 
 }
